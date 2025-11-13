8905f2aa14252e20966f525d9e5a27d87f1128db
sukhodolskyivg24-sudo <sukhodolskyi_vg24@nuwm.edu.ua>
2025-11-13 15:57:19 +0200

    add new django-app

---
+++ sukhodolskyi/app_blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Створіть свої представлення тут.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
